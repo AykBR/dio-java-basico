d21987604cbfad366a531c233dfde68c49e0f419
Ayk <75586907+AykBR@users.noreply.github.com>
2024-07-11 15:45:41 -0300

    adicionando projeto de modelagem java

---
+++ POO - Desafios/Desafio Iphone/desafio-iphone/src/navegador/Navegador.java
@@ -0,0 +1,10 @@
+package navegador;
+
+public class Navegador {
+        public void exibirPagina(String url);
+        public void adicionarNovaAba();
+        public void atualizarPagina();
+    
+        }
+    
+
---
+++ POO - Desafios/Desafio Iphone/desafio-iphone/src/reprodutor/ReprodutorMusical.java
@@ -0,0 +1,8 @@
+package reprodutor;
+public class ReprodutorMusical {
+  
+    public void tocar();
+    public void pausar();
+    public void selecionarMusica(String musica);
+
+    }
